e2967d94a3d613e4fb1162d6daec37e12ae03be8
Jos Huiting <jos.huiting@eye.security>
2024-12-04 07:16:24 +0100

    Add day4

---
+++ day4/main.go
@@ -0,0 +1,100 @@
+package day
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func init() {
+	input = strings.TrimRight(input, "\n")
+	if len(input) == 0 {
+		panic("The input.txt file is empty")
+	}
+}
+
+func part1(input string) int {
+	xmasMatches := 0
+	var columns = make(map[int][]string)
+
+	for lineNumber, line := range strings.Split(input, "\n") {
+		xmasMatches += strings.Count(line, "XMAS")
+		xmasMatches += strings.Count(line, "SAMX")
+
+		for index, character := range strings.Split(line, "") {
+			columns[index] = append(columns[index], character)
+
+			// Backward
+			if lineNumber >= 3 && index >= 3 && character == "X" &&
+				columns[index-1][lineNumber-1] == "M" &&
+				columns[index-2][lineNumber-2] == "A" &&
+				columns[index-3][lineNumber-3] == "S" {
+				xmasMatches += 1
+			}
+			if lineNumber >= 3 && index >= 3 && character == "S" &&
+				columns[index-1][lineNumber-1] == "A" &&
+				columns[index-2][lineNumber-2] == "M" &&
+				columns[index-3][lineNumber-3] == "X" {
+				xmasMatches += 1
+			}
+
+			// Forward
+			if lineNumber >= 3 && index <= len(line)-4 && character == "X" &&
+				columns[index+1][lineNumber-1] == "M" &&
+				columns[index+2][lineNumber-2] == "A" &&
+				columns[index+3][lineNumber-3] == "S" {
+				xmasMatches += 1
+				fmt.Println(fmt.Sprintf("XMAS schuin hit, line %d", lineNumber))
+			}
+			if lineNumber >= 3 && index <= len(line)-4 && character == "S" &&
+				columns[index+1][lineNumber-1] == "A" &&
+				columns[index+2][lineNumber-2] == "M" &&
+				columns[index+3][lineNumber-3] == "X" {
+				xmasMatches += 1
+				fmt.Println(fmt.Sprintf("SAMX schuin hit, line %d", lineNumber))
+			}
+		}
+	}
+
+	for _, column := range columns {
+		xmasMatches += strings.Count(strings.Join(column, ""), "XMAS")
+		xmasMatches += strings.Count(strings.Join(column, ""), "SAMX")
+	}
+
+	return xmasMatches
+}
+
+func part2(input string) int {
+	xmasMatches := 0
+	var columns = make(map[int][]string)
+
+	for lineNumber, line := range strings.Split(input, "\n") {
+		for index, character := range strings.Split(line, "") {
+			columns[index] = append(columns[index], character)
+
+			// Backward
+			if (lineNumber >= 2 && index >= 2 && character == "M" &&
+				columns[index-1][lineNumber-1] == "A" &&
+				columns[index-2][lineNumber-2] == "S") ||
+				(lineNumber >= 2 && index >= 2 && character == "S" &&
+					columns[index-1][lineNumber-1] == "A" &&
+					columns[index-2][lineNumber-2] == "M") {
+
+				if line[index-2] == 'S' &&
+					columns[index-1][lineNumber-1] == "A" &&
+					columns[index][lineNumber-2] == "M" {
+					xmasMatches += 1
+				} else if line[index-2] == 'M' &&
+					columns[index-1][lineNumber-1] == "A" &&
+					columns[index][lineNumber-2] == "S" {
+					xmasMatches += 1
+				}
+			}
+		}
+	}
+
+	return xmasMatches
+}
---
+++ day4/main_test.go
@@ -0,0 +1,27 @@
+package day
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+const example = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`
+
+func Test_part1(t *testing.T) {
+	assert.Equal(t, 18, part1(example))
+	assert.Equal(t, 2662, part1(input))
+}
+
+func Test_part2(t *testing.T) {
+	assert.Equal(t, 9, part2(example))
+	assert.Equal(t, 2662, part2(input))
+}
