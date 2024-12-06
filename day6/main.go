308f302e135b5e2b1f3849a2b1f3b3c0bde09bca
Jos Huiting <jos.huiting@eye.security>
2024-12-06 10:49:33 +0100

    Optimize day6

--- day6/main.go
+++ day6/main.go
@@ -17,20 +17,20 @@ func init() {
 }
 
 type Position struct {
-	X int
-	Y int
+	x int
+	y int
 }
 
 func (position Position) ChangePosition(direction Direction) Position {
 	switch direction {
 	case Up:
-		return Position{X: position.X, Y: position.Y - 1}
+		return Position{x: position.x, y: position.y - 1}
 	case Down:
-		return Position{X: position.X, Y: position.Y + 1}
+		return Position{x: position.x, y: position.y + 1}
 	case Left:
-		return Position{X: position.X - 1, Y: position.Y}
+		return Position{x: position.x - 1, y: position.y}
 	case Right:
-		return Position{X: position.X + 1, Y: position.Y}
+		return Position{x: position.x + 1, y: position.y}
 	}
 
 	panic("Invalid direction")
@@ -45,6 +45,12 @@ const (
 	Right Direction = 4
 )
 
+const (
+	UpChar       = "^"
+	ObstacleChar = "#"
+	PathChar     = "."
+)
+
 func part1(input string) int {
 	grid, position := createGrid(input)
 
@@ -56,14 +62,17 @@ func part2(input string) int {
 	grid, position := createGrid(input)
 
 	loops := 0
+	positionsOnPath, _ := walkGrid(position, Up, grid)
+
 	for y, row := range grid {
 		for x, cell := range row {
-			if cell == "." {
-				grid[y][x] = "#"
+			if cell == PathChar && slices.Index(positionsOnPath, Position{x, y}) != -1 {
+				// Just try when it's on the guard's path
+				grid[y][x] = ObstacleChar
 				if _, isLoop := walkGrid(position, Up, grid); isLoop {
 					loops += 1
 				}
-				grid[y][x] = "."
+				grid[y][x] = PathChar
 			}
 
 		}
@@ -77,7 +86,7 @@ func createGrid(input string) ([][]string, Position) {
 	grid := make([][]string, 0)
 	for lineNumber, line := range strings.Split(input, "\n") {
 		grid = append(grid, strings.Split(line, ""))
-		if result := slices.Index(grid[lineNumber], "^"); result != -1 {
+		if result := slices.Index(grid[lineNumber], UpChar); result != -1 {
 			position = Position{result, lineNumber}
 		}
 	}
@@ -93,22 +102,22 @@ func walkGrid(position Position, direction Direction, grid [][]string) ([]Positi
 		newPosition := position.ChangePosition(direction)
 		positionOccurrences[newPosition] += 1
 
-		if (newPosition.X < 0 || newPosition.X >= len(grid[0])) || (newPosition.Y < 0 || newPosition.Y >= len(grid)) {
+		if (newPosition.x < 0 || newPosition.x >= len(grid[0])) || (newPosition.y < 0 || newPosition.y >= len(grid)) {
 			return visitedPositions, false
 		} else if positionOccurrences[newPosition] > 5 {
 			return visitedPositions, true
 		}
 
-		if direction == Up && grid[newPosition.Y][newPosition.X] == "#" {
+		if direction == Up && grid[newPosition.y][newPosition.x] == ObstacleChar {
 			direction = Right
 			continue
-		} else if direction == Right && grid[newPosition.Y][newPosition.X] == "#" {
+		} else if direction == Right && grid[newPosition.y][newPosition.x] == ObstacleChar {
 			direction = Down
 			continue
-		} else if direction == Down && grid[newPosition.Y][newPosition.X] == "#" {
+		} else if direction == Down && grid[newPosition.y][newPosition.x] == ObstacleChar {
 			direction = Left
 			continue
-		} else if direction == Left && grid[newPosition.Y][newPosition.X] == "#" {
+		} else if direction == Left && grid[newPosition.y][newPosition.x] == ObstacleChar {
 			direction = Up
 			continue
 		}
